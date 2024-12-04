c753fa4f5164a8ff5d6f28b4259403f7ce9782c6
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2024-12-04 11:01:15 +0530

    Initialize the base converter library

---
+++ Base_Converter/library.cpp
@@ -0,0 +1,11 @@
+#include "library.h"
+#include <iostream>
+using namespace std;
+
+void hello() {
+    cout << "Hello, World!" << endl;
+}
+int main() {
+    hello();
+    return 0;
+}
---
+++ Base_Converter/library.h
@@ -0,0 +1,6 @@
+#ifndef BASE_CONVERTER_LIBRARY_H
+#define BASE_CONVERTER_LIBRARY_H
+
+void hello();
+
+#endif //BASE_CONVERTER_LIBRARY_H
